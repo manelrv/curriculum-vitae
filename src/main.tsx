4ac7e2d0d3c9a838d0200654a408654fc00257c5
manelrv <mromero@labelgrup.com>
2023-04-12 08:41:19 +0200

    Added clariry

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import './index.css'
 import install from '@twind/with-react'
 import config from './twind.config'
 import { clarity } from 'react-microsoft-clarity'
-install(config)
+install(config) // twind
 clarity.init('gn9c8tkj6y')
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
