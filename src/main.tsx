5e0efba955fce6a6bf06bd7c6aeb3aded8133c04
manelrv <mromero@labelgrup.com>
2023-04-12 08:40:46 +0200

    added clarity

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,9 @@ import App from './App'
 import './index.css'
 import install from '@twind/with-react'
 import config from './twind.config'
+import { clarity } from 'react-microsoft-clarity'
 install(config)
-
+clarity.init('gn9c8tkj6y')
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
